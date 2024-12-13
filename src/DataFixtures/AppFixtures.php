7a88eec9623ebce5999106b4cf5024e54054c256
Laetitia Mantel <laetitia.lecroart@gmail.com>
2024-12-13 11:29:43 +0100

    feat : fixture and db ok

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,54 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Ingredient;
+use App\Entity\Recipe;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
 class AppFixtures extends Fixture
 {
+    private $recipes = [];
+
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $faker = \Faker\Factory::create();
+        $faker->addProvider(new \FakerRestaurant\Provider\fr_FR\Restaurant($faker));
 
+        // Generator
+        $faker->foodName();      // A random Food Name
+        $faker->beverageName();  // A random Beverage Name
+        $faker->dairyName();  // A random Dairy Name
+        $faker->vegetableName();  // A random Vegetable Name
+        $faker->fruitName();  // A random Fruit Name
+        $faker->meatName();  // A random Meat Name
+        $faker->sauceName();  // A random Sauce Name
+
+        // Tableau des unités de mesures
+        $unit = [ "g","cl","CaS","CaC","pièce",];
+
+        
+        // Création de 50 recettes avec 5 à 10 ingredients par recette
+        for ($i = 0; $i < 50; $i ++) {
+            $recipe = new Recipe;
+            $recipe->setName($faker->foodName());
+            $recipe->setPreparation($faker->text());
+            $manager->persist($recipe);    
+            $this->recipes[] = $recipe;       
+        };
+        
+        // Création de 150 ingerdients
+        for ($i = 0; $i < 150; $i++) {
+            $ingredient = new Ingredient;
+            $ingredient->setName($faker->vegetableName());
+            $ingredient->setUnit($unit[array_rand($unit)]);
+            for ($j = 0; $j < random_int(5, 10); $j ++){
+                $ingredient->addRecipe($faker->unique()->randomElement($this->recipes));
+            }
+            $manager->persist($ingredient);
+            $ingredientInDb [] = $ingredient;
+        };
+                
         $manager->flush();
     }
 }
